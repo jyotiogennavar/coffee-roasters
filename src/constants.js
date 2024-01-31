1de02648c658127b9b4db4f47fd1156274395980
Jyoti <jyoti.ogennavar@xecurify.com>
2024-01-31 09:48:47 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,28 @@
+export const FONT_FAMILY = {
+  fontHeading: 'Fraunces, serif',
+  fontText: 'Barlow, sans-serif',
+};
+
+export const COLORS = {
+  darkCyan: 'hsl(179°, 81%, 29%)',
+  darkGreyBlue: 'hsl(215°, 19%, 25%)',
+  paleOrange: 'hsl(25°, 94%, 86%)',
+  lightCream: 'hsl(43°, 78%, 98%)',
+  grey: 'hsl(215°, 5%, 54%)',
+};
+
+export const WEIGHTS = {
+  normal: 400,
+  semibold: 700,
+  bold: 900,
+};
+
+export const FONT_SIZES = {
+  100: '0.75rem',
+  200: '1rem',
+  300: '1.5rem',
+  400: '2rem',
+  500: '2.5rem',
+  600: '4rem',
+  button: '1.125rem', 
+};
