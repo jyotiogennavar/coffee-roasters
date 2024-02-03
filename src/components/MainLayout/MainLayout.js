dbfefa1e20268ce1477a4af14548020cfc0749b1
Jyoti <jyoti.ogennavar@xecurify.com>
2024-02-03 19:36:03 +0530

    fixing errors

--- src/components/MainLayout/MainLayout.js
+++ src/components/MainLayout/MainLayout.js
@@ -4,15 +4,16 @@ import { motion } from 'framer-motion';
 import GlobalStyles from '../GlobalStyles';
 import {COLORS, WEIGHTS, FONT_SIZES, FONT_FAMILY} from '../../constants';
 
-import Navbar from '../Navbar/Navbar';
-import Footer from '../Footer/Footer';
+import Navbar from '../Navbar';
+
+
 
 const MainLayout = ({children}) => {
   return (
     <MainWrapper>
       <Navbar/>
       <div>{children}</div> 
-      <Footer/>
+
     </MainWrapper>
   )
 }
